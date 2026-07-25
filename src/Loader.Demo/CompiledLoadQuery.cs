2a2acbcb8adf856c6553256a1d7dc86c58324738
MikeKondr99 <MikeKondr99@gmail.com>
2026-07-25 15:45:53 +0300

    удалил demo

--- src/Loader.Demo/CompiledLoadQuery.cs
+++
@@ -1,8 +0,0 @@
-namespace Loader.Demo;
-
-internal sealed record CompiledLoadQuery
-{
-    public required string Sql { get; init; }
-
-    public required IReadOnlyList<string> LogicalNames { get; init; }
-}
